1b5c598829655701b925ceec8dc7963f1d4bb86f
Isabela Müller <isabelamuller1@hotmail.com>
2021-12-05 00:12:12 -0300

    a

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<Integer> t = new HashSet<Integer>();
+        t.add(9);
+        t.add(4);
+        t.add(89);
+        t.add(2);
+        t.size();
+
+        int x = t.size();
+        System.out.println(x);
+        }
+}
+
